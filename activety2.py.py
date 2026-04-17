50faf9156c517cdee916dae207fb057094b33942
ann7897 <hubertprakash@hotmail.com>
2026-04-17 20:19:02 +0100

    Add files via upload

---
+++ activety2.py.py
@@ -0,0 +1,16 @@
+
+class vehicle
+
+
+    def __init__(slef, max_speed, mileage):
+
+
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+modelx = vehicle(240, 18)
+
+
+print("model max speed:",modelx.mac_speed)
+print("model mileage:", modelx.mileage)
